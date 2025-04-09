5efb525951442641cb9eb4b020f85da9272fa42f
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-04-09 12:49:24 -0700

    merge 2 sorted lists

--- p21.py
+++ p21.py
@@ -1,21 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def isValid(self, s: str) -> bool:
-        arr = []
+    
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        map = {
-            "{": "}",
-            "[": "]",
-            "(": ")"
-        }
+        if not list1 and list2:
+            return list2
+        elif not list2 and list1:
+            return list1
+        elif not list1 and not list2:
+            return None
+        else: 
+            tmp = None
+            final = None
 
-        for char in s:
-            if len(arr) == 0:
-                arr.append(char)
+            if list1.val <= list2.val:
+                tmp = list1
+                list1 = list1.next
             else:
-                if arr[len(arr)-1] in map and map[arr[len(arr)-1]] == char:
-                    arr.pop()
-                else:
-                    arr.append(char)
+                tmp = list2
+                list2 = list2.next
 
-        return True if len(arr) == 0 else False 
+            final = tmp
             
+            while list1 and list2:
+                if list1.val <= list2.val:
+                    tmp.next = list1
+                    list1 = list1.next
+                else:
+                    tmp.next = list2
+                    list2 = list2.next
+
+                tmp = tmp.next
+
+            while list1:
+                tmp.next = list1
+                list1 = list1.next
+                tmp = tmp.next
+
+            while list2:
+                tmp.next = list2
+                list2 = list2.next
+                tmp = tmp.next
+
+            return final
+
+            
+
+
+
+        
