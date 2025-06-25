8f7f453ad2b763732ffc4a5f9faccd806efeedb9
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-06-24 23:59:44 -0700

    Add Two Numbers

--- p2.py
+++ p2.py
@@ -5,39 +5,21 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        final = ListNode(-1)
+        current = final
+        remaining = 0
 
-        if not  l1:
-            return l2
-        elif not l2:
-            return l1
-        else:
-            sub = ListNode(0)
-            final = sub
+        while l1 or l2 or remaining:
 
-            first = 0
-            second = 0
-            whole = 0
-            first_remainder = 0
-            second_remainder = 0
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + remaining
 
-            while l1 or l2 or second_remainder:
-                if l1:
-                    first = l1.val
-                    l1 = l1.next
-                else:
-                    first = 0
+            current.next = ListNode(total%10)
+            remaining = ListNode(total//10)
+            current = current.next
 
-                if l2:
-                    second = l2.val
-                    l2 = l2.next
-                else:
-                    second = 0
-
-                whole = (first + second + second_remainder) // 10
-                first_remainder = (first + second + second_remainder) % 10
-                second_remainder = whole % 10
-                sub.next = ListNode(first_remainder)
-                sub = sub.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
 
         return final.next
-
