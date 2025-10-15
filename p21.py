d3a5b3fa414ac5dfe51144991e08372f66928f76
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-14 20:12:40 -0700

    Update p21.py

--- p21.py
+++ p21.py
@@ -3,53 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1000)  # dummy node
+        tail = dummy             # tail tracks end of merged list
 
-        if not list1 and list2:
-            return list2
-        elif not list2 and list1:
-            return list1
-        elif not list1 and not list2:
-            return None
-        else: 
-            tmp = None
-            final = None
-
-            if list1.val <= list2.val:
-                tmp = list1
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
                 list1 = list1.next
             else:
-                tmp = list2
+                tail.next = list2
                 list2 = list2.next
+            tail = tail.next  # move tail forward
 
-            final = tmp
-            
-            while list1 and list2:
-                if list1.val <= list2.val:
-                    tmp.next = list1
-                    list1 = list1.next
-                else:
-                    tmp.next = list2
-                    list2 = list2.next
+        # Attach any remaining nodes
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
 
-                tmp = tmp.next
-
-            while list1:
-                tmp.next = list1
-                list1 = list1.next
-                tmp = tmp.next
-
-            while list2:
-                tmp.next = list2
-                list2 = list2.next
-                tmp = tmp.next
-
-            return final
-
-            
-
-
-
-        
+        return dummy.next
