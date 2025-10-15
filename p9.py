a074ed3230f8965f4a7029c22c634bfff1e6bf03
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-14 17:08:59 -0700

    Update p9.py

--- p9.py
+++ p9.py
@@ -1,14 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        value = x
-        self = 0
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
 
-        while x > 0:
-            last_digit = x % 10
-            self = self*10 + last_digit
+        half = 0
+        while x > half:
+            half = half * 10 + x % 10
             x //= 10
 
-        if self == value:
-            return True 
-        else:
-            return False
+        return half == x or x == (half // 10)
