0562f4b5879bdb029b01e148b8625da456ea4a69
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-27 16:29:39 -0700

    Update p35.py

--- p35.py
+++ p35.py
@@ -1,14 +1,11 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        for ct in range(len(nums)):
-            if nums[ct] == target:
-                return ct
-
-        if target < nums[0]:
-            return 0
-        if target > nums[len(nums)-1]:
-            return len(nums)
-        else:
-            for ct in range(1,len(nums)):
-                if target > nums[ct-1] and target <= nums[ct]:
-                    return ct
+    def removeElement(self, nums: List[int], val: int) -> int:
+        final = nums.copy()
+        length = len(final)
+        a = 0
+        for i in range(length):
+            if final[i] != val:
+                nums[a] = final[i]
+                a += 1
+        return a
+            
