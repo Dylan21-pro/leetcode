b85239dc23597ba8b98f9ba2f92115b66a567b4d
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-27 16:37:36 -0700

    Update p58.py

--- p58.py
+++ p58.py
@@ -1,5 +1,4 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        string = s.rstrip()
-        arr = string.split(' ')
-        return len(arr[len(arr)-1])
+        final = s.strip().split(" ")
+        return len(final.pop())
