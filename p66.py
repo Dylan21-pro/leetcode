366cb080ee39cc1ff2042296f8a8711dd0096a41
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-27 16:51:50 -0700

    Update p66.py

--- p66.py
+++ p66.py
@@ -1,23 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-
-        if digits[len(digits)-1]+1 < 10:
-            digits[len(digits)-1] += 1
-        else:
-            whole = 0
-            first_remainder = 0
-            carry = 1
-            count = len(digits)-1
-            while carry != 0:
-                if count != 0:
-                    whole = (digits[count] + carry) // 10
-                    first_remainder = (digits[count] + carry) % 10
-                    digits[count] = first_remainder
-                    carry = whole % 10
-                else:
-                    digits.insert(0,carry)
-                    break
-                count -= 1
-
-        return digits
+        carry = 0
+        left = 0
+        final = []
+        last = len(digits)-1
+        digits[last] += 1
+        for i in range(last, -1, -1):
+            final.insert(0,(digits[i] + carry) % 10)
+            carry = (digits[i] + carry) // 10
+        if carry != 0:
+            final.insert(0,carry)
+        return final
         
