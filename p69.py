1d302b1530702e25c0f03a648b94368375771f2d
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-28 14:50:17 -0700

    Update p69.py

--- p69.py
+++ p69.py
@@ -1,4 +1,21 @@
+import math
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-
-        return math.floor(math.sqrt(x))
+        if x == 0:
+            return 0
+        elif x == 1 or x == 2 :
+            return 1
+       
+        left = 0
+        right = x
+        while left < right:
+            mid = math.ceil((right-left)/2 + left)
+            double = mid * mid
+            if double == x or (double < x and (mid+1)**2 > x):
+                return mid
+            elif double < x:
+                left = mid
+            elif double > x:
+                right = mid
+        return mid
