50dfe051bcdd91d79d97fbd3b8fff37e24de6c93
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-29 15:03:51 -0700

    Update p88.py

--- p88.py
+++ p88.py
@@ -1,29 +1,9 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        if m+n == 1:
-            if m == 0:
-                nums1[0]=nums2[0]
-        else:
-            arr = nums1[:m]
-            ct1 = 0
-            ct2 = 0
-            while ct1 < m and ct2 < n:
-                if arr[ct1] > nums2[ct2]:
-                    nums1[ct1+ct2] = nums2[ct2]
-                    ct2 += 1
-                elif arr[ct1] <= nums2[ct2]:
-                    nums1[ct1+ct2] = arr[ct1]
-                    ct1 += 1 
-
-            while ct1 < m:
-                nums1[ct1+ct2] = arr[ct1]
-                ct1 += 1
-
-            while ct2 < n:
-                nums1[ct1+ct2] = nums2[ct2]
-                ct2 += 1
-
-                
-
-                 
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums2.extend(nums1[0:m])
+        nums1.clear()
+        nums1.extend(sorted(nums2))
         
