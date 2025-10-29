836334058442602d58ea2f326f50c63f44e173db
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-28 21:24:41 -0700

    Update p70.py

--- p70.py
+++ p70.py
@@ -1,27 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        count = 0
-        this_number = 0
-        prev_number = 0
-        
         if n == 1:
             return 1
-        if n == 2:
+        elif n == 2:
             return 2
         elif n == 3:
             return 3
         else:
-            this_number = 3
-            prev_number = 2
-            count = 3
-            while count < n:
-                num = this_number
-                this_number += prev_number
-                prev_number = num
+            count = 0
+            prev = 2
+            current = 3
+            while count < n - 2:
+                prev, current = current, prev
+                current += prev
                 count += 1
-            return this_number
-
-
-    def fibonacci(a, b):
-
-        return 0
+            return prev
