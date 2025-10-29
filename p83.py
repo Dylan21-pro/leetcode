5bd7035f2a4aba0ae65f5290876e9e54a465097b
Dylan21-pro <dylandang.ngo2001@gmail.com>
2025-10-29 14:07:56 -0700

    Update p83.py

--- p83.py
+++ p83.py
@@ -3,61 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-'''
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        first = ListNode(101)
-        second = ListNode(-101)
-        first.next = second
-
-        if head == None or head.next == None:
+        if not head:
             return head
-        else:
-            while head != None:
 
-                if second.val != head.val :
-                    second.next = head
-                    second = second.next
-                
-                head = head.next
-            second.next = None
+        current = head
 
-            return first.next.next
-                
-
-                
-
-        print(first.next)
-        return first.next.next
-'''
-
-class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        while current and current.next:
+            if current.val != current.next.val:
+                current = current.next
+            else:
+                current.next = current.next.next
+        return head
         
-        realHead = ListNode(101)
-        secondHead = ListNode(101)
-        realHead.next = secondHead
-        newSet = set()
-        count = 0 
-        
-        if head == None or head.next == None:
-            return head
-        else:
-            while head != None:
-                newSet.add(head.val)
-                if len(newSet) > count:
-                    count += 1
-                    secondHead.next = ListNode(head.val)
-                    secondHead = secondHead.next
-
-                head = head.next
-                
-        return realHead.next.next
-
-
-
-
-
-
-
-                
